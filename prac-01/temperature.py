4acb04a07f94eb6cf106685ce12b21b6a21dd805
laijunren <2924068177@qq.com>
2020-11-26 15:09:29 +0800

    Conver Fahrenheit to Celsius

---
+++ prac-01/temperature.py
@@ -0,0 +1,18 @@
+MENU = """C - Convert Celsius to Fahrenheit
+F - Convert Fahrenheit to Celsius
+Q - Quit"""
+print(MENU)
+choice = input(">>> ").upper()
+while choice != "Q":
+    if choice == "C":
+        celsius = float(input("Celsius: "))
+        fahrenheit = celsius * 9.0 / 5 + 32
+        print("Result: {:.2f} F".format(fahrenheit))
+    elif choice == "F":
+        celsius = 5 / 9 * (fahrenheit - 32)
+        print("Result: {:.2f} C".format(celsius))
+    else:
+        print("Invalid option")
+    print(MENU)
+    choice = input(">>> ").upper()
+print("Thank you.")
