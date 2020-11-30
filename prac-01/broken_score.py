9f076ea752b079f88fa556bb4954c88998a9cd1d
laijunren <2924068177@qq.com>
2020-11-30 20:08:40 +0800

    Resolve grade issues in code

--- prac-01/broken_score.py
+++ prac-01/broken_score.py
@@ -1,15 +1,26 @@
-"""
-CP1404/CP5632 - Practical - Suggested Solution
-Fixed program to determine score status
-"""
+import random
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-else:
-    if score > 90:
-        print("Excellent")
-    elif score >= 50:
-        print("Passable")
-    else:
-        print("Bad")
+
+def main():
+    score = int(input('Enter your score: '))
+    result = get_result(score)
+
+    random_score = random.randint(0, 100)
+    print('Random score:', random_score)
+    result = get_result(random_score)
+
+
+def get_result(score):
+    if score < 50:
+        print('Fail')
+    elif score >= 50 and score <= 64:
+        print('Pass')
+    elif score >= 65 and score <= 74:
+        print('Pass with Credit')
+    elif score >= 75 and score <= 84:
+        print('Pass with Distinction')
+    elif score >= 85 and score <= 100:
+        print('Pass with High Distinction')
+
+
+main()
