c3c38f7a76690b97e5b5c6a444f60cd6999898f0
laijunren <2924068177@qq.com>
2021-01-06 09:04:11 +0800

    Change the code

--- prac-07/box_layout_demo.py
+++ prac-07/box_layout_demo.py
@@ -3,10 +3,19 @@ from kivy.lang import Builder
 
 
 class BoxLayoutDemo(App):
+    def clear(self):
+        self.root.ids.output_label.text = ""
+        self.root.ids.input_name.text = ''
+
     def build(self):
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
+
+    def handle_greet(self):
+        print('greet')
+        print("test")
+        self.root.ids.output_label.text = "Hello " + self.root.ids.input_name.text
 
 
 BoxLayoutDemo().run()
